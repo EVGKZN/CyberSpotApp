b7d8d0b52f772b8ff7f918f00566632dac1ee4d9
Evgeniy Kuzmin <e.kuzmin.kzn@gmail.com>
2020-03-18 14:27:47 +0300

    Initial Commit

---
+++ CyberSpotApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CyberSpotApp
+//
+//  Created by Евгений on 18.03.2020.
+//  Copyright © 2020 Евгений Кузьмин. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
